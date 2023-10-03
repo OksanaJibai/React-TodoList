dbd313ab621e1444f1c406bbace92bfad9c3b511
Oksana <oks49442780@gmail.com>
2023-10-03 21:55:20 +0200

    add formik and todoForm.jsx

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 			{/* eslint-disable-next-line react/react-in-jsx-scope */}
 			<h1 className="text-center mt-5 mb-5">TODO LIST</h1>
 			{/* eslint-disable-next-line react/react-in-jsx-scope */}
-			< TodoList />
+			<TodoList/>
 		</>
 	);
 }
